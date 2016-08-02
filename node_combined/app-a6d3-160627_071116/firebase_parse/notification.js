c28329ba53c59dbae7e710728b5c891aa49b136b
Shariar Shaikot <mail2shariar@gmail.com>
2016-08-02 22:41:12 +0600

    notification issue fixed

--- node_combined/app-a6d3-160627_071116/firebase_parse/notification.js
+++ node_combined/app-a6d3-160627_071116/firebase_parse/notification.js
@@ -34,7 +34,7 @@ exports.locationService = function(parse) {
     devicesRef.
         on("child_added", function(deviceSnapshot) {
             parseLoc(deviceSnapshot, parse);
-    });
+        });
 
     /** Will fire when a new object add to device **/
     devicesRef.
@@ -78,35 +78,94 @@ function parseLoc (deviceSnapshot, parse)
                             settingsRef.child(device.devicetoken).once('value', function (setSnap) {
                                 var settings = setSnap.val();
 
-                                console.log(JSON.stringify(settings));
 
                                 settings = settings == null ? {enableNotification: true} : settings;
 
                                 if ((now > event.start || now < event.end)
                                     && settings.enableNotification) {
 
-
-                                    console.log("gotcha");
                                     distance = require('./connectfirebase').distance(location.latitude,
                                         location.longitude, device.latitude, device.longitude);
                                     radius = location.radius || 20;
 
 
-                                    console.log("distance: "+distance + " device: "+ device.devicetoken.substring(0, 8)
-                                        +" radius: "+ radius);
+                                    //console.log("distance: "+distance + " device: "+ device.devicetoken.substring(0, 8)
+                                    //    +" radius: "+ radius);
 
                                     if (distance < radius) {
+
+                                        console.log("In the distance");
 
 
                                         notificationsRef.orderByChild('deviceid').
                                             equalTo(child_device.key()).
                                             once('value', function (nSnapshot) {
 
-                                                notifications = nSnapshot.val();
-                                                message = getMessage(location.message, location.name, distance);
 
 
-                                                if (!notifications) {
+                                                notifications = nSnapshot.val(); // Create the notification snapshot of that device
+                                                message = getMessage(location.message, location.name, distance); // Create the msg
+
+
+                                                console.log("@@@@@@@@@@@@@@@@@@@ ", child_location.key());
+
+                                                if (nSnapshot.numChildren()>0) { //If notification obj found for this device
+
+                                                    //Iterate through notifications
+                                                    var i = 0; //initiate the index for iteration
+                                                    var availableOn = new Array(); // Create an empty array for location store
+                                                    nSnapshot.forEach(function (each) {
+                                                        ++i; //increase index
+                                                        var notification = each.val();
+                                                        availableOn.push(notification.locationid); //push all available locationid
+                                                        if (notification.locationid == child_location.key() &&
+                                                            notification.deviceid==child_device.key()) {
+                                                            //If the current location matches with the notification
+                                                            //Checks time availability
+                                                            if ((notification.pushdate + (24 * 3600 * 1000)) <
+                                                                (new Date().getTime())) {
+                                                                console.log ("Should get a new notification");
+
+                                                                parse.sendNotificationWithInstallId(device.devicetoken,
+                                                                    message, function (flag) {
+                                                                        if (flag) {
+                                                                            notificationsRef.push().set({
+                                                                                deviceid: child_device.key(),
+                                                                                locationid: child_location.key(),
+                                                                                pushdate: new Date().getTime()
+                                                                            });
+                                                                        } else {
+                                                                            console.log("parse error");
+                                                                        }
+                                                                    });
+
+                                                            }
+                                                        }
+
+                                                        if (i==nSnapshot.numChildren()) { //if iteration ends, run this
+                                                            if (availableOn.indexOf(child_location.key())==-1) {
+                                                                //if location is not found on the array
+                                                                //that means, this location is new point of interest
+                                                                parse.sendNotificationWithInstallId(device.devicetoken,
+                                                                    message, function (flag) {
+                                                                        if (flag) {
+                                                                            notificationsRef.push().set({
+                                                                                deviceid: child_device.key(),
+                                                                                locationid: child_location.key(),
+                                                                                pushdate: new Date().getTime()
+                                                                            });
+                                                                        } else {
+                                                                            console.log("parse error");
+                                                                        }
+                                                                    });
+
+                                                            }
+                                                        }
+                                                    });
+                                                } else {
+                                                    // A very new notification will be sent
+                                                    console.log("Ready ....");
+
                                                     parse.sendNotificationWithInstallId(device.devicetoken,
                                                         message, function (flag) {
                                                             if (flag) {
@@ -119,59 +178,10 @@ function parseLoc (deviceSnapshot, parse)
                                                                 console.log("parse error");
                                                             }
                                                         });
-                                                } else {
 
-                                                    nSnapshot.forEach(function (eachNoty) {
-
-                                                        var notification = eachNoty.val();
-
-                                                        if (notification.locationid == child_location.key() &&
-                                                            notification.deviceid == child_device.key()) {
-
-
-                                                            console.log("pathway to notification of: " + location.name);
-
-                                                            newOne = true;
-
-                                                            if ((notification.pushdate + (24 * 3600 * 1000)) <
-                                                                (new Date().getTime())) {
-
-                                                                parse.sendNotificationWithInstallId(device.devicetoken,
-                                                                    message, function (flag) {
-                                                                        if (flag) {
-                                                                            var updateNotificationRef = new Firebase(staticData.firebaseUrl +
-                                                                                '/notifications/' + eachNoty.key());
-                                                                            updateNotificationRef.update({pushdate: new Date().getTime()},
-                                                                                function (err) {
-                                                                                });
-                                                                        } else {
-                                                                            console.log("parse error");
-                                                                        }
-                                                                    });
-
-                                                            }
-
-                                                        }
-
-                                                    });
-
-                                                    if (!newOne) {
-                                                        newOne = false;
-                                                        console.log("Why I am?");
-                                                        parse.sendNotificationWithInstallId(device.devicetoken,
-                                                            message, function (flag) {
-                                                                if (flag) {
-                                                                    notificationsRef.push().set({
-                                                                        deviceid: child_device.key(),
-                                                                        locationid: child_location.key(),
-                                                                        pushdate: new Date().getTime()
-                                                                    });
-                                                                } else {
-                                                                    console.log("parse error");
-                                                                }
-                                                            });
-                                                    }
                                                 }
+
+
                                             });
                                     }
 
